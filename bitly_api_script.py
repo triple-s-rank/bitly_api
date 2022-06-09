f6db02ca9ab8e16d55547392db0a3eae379dc77c
triple-s-rank <specialforwar@icloud.com>
2022-06-10 01:09:11 +0300

    initial script commit

---
+++ bitly_api_script.py
@@ -0,0 +1,54 @@
+import argparse
+import os
+
+import requests
+from dotenv import load_dotenv
+from urllib.parse import urlparse
+
+
+def shorten_url(link, headers):
+    data = {'long_url': f'http://{link}'}
+    response = requests.post(url='https://api-ssl.bitly.com/v4/shorten', headers=headers, json=data)
+    response.raise_for_status()
+    return response.json()['link']
+
+
+def count_clicks(link, headers):
+    response = requests.get(url=f'https://api-ssl.bitly.com/v4/bitlinks/{link}/clicks/summary', headers=headers)
+    response.raise_for_status()
+    return response.json()['total_clicks']
+
+
+def is_bitlink(link, headers):
+    response = requests.get(url=f'https://api-ssl.bitly.com/v4/bitlinks/{link}', headers=headers)
+    return response.ok
+
+
+def main():
+    load_dotenv()
+
+    access_token = os.getenv("ACCESS_TOKEN")
+    headers = {'Authorization': f'Bearer {access_token}'}
+    parser = argparse.ArgumentParser(description="Transform url to bitly link and counts clicks on it.")
+    parser.add_argument(
+        "url",
+        type=str,
+        help="use 'python main.py {url}'"
+        )
+    args = parser.parse_args()
+    url = args.url
+    parsed_url = urlparse(url)
+    serialized_url = parsed_url.netloc + parsed_url.path
+
+    try:
+        if is_bitlink(serialized_url, headers):
+            print('По вашей ссылке прошли', count_clicks(serialized_url, headers), 'раз(а).')
+        else:
+            bitlink = shorten_url(serialized_url, headers)
+            print('Битлинк', bitlink)
+    except requests.HTTPError:
+        print('Вы ввели неверную ссылку.')
+
+
+if __name__ == '__main__':
+    main()
